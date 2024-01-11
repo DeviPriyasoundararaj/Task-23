3963a00a8aefe1cdd0325509db8c9d6cf970201b
Devi Priya S <114299272+DeviPriyasoundararaj@users.noreply.github.com>
2024-01-11 20:05:45 +0530

    Create Edituser.js

---
+++ Edituser.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Form.css';
+
+const EditUser = () => {
+  return (
+    <div className="form-container">
+      <h1>Edit User</h1>
+      <form>
+        {/* Add form elements for user editing */}
+        <button type="submit">Update</button>
+      </form>
+    </div>
+  );
+};
+
+export default EditUser;
