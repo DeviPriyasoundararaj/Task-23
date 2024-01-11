519008a56a5be51b04af1db8e7697b0c1d4e555f
Devi Priya S <114299272+DeviPriyasoundararaj@users.noreply.github.com>
2024-01-11 20:07:40 +0530

    Create Editprofile.js

---
+++ Editprofile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Form.css';
+
+const EditProfile = () => {
+  return (
+    <div className="form-container">
+      <h1>Edit Profile</h1>
+      <form>
+        {/* Add form elements for profile editing */}
+        <button type="submit">Update</button>
+      </form>
+    </div>
+  );
+};
+
+export default EditProfile;
